dd27a6b604aad96e5c36399084cd612de68bc423
m4ssive4tt4ck <rahelkempf@gmx.ch>
2022-04-16 12:58:58 +0200

    rmv unneccesary import

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from distutils import file_util
 from signal import signal, SIGPIPE, SIG_DFL 
 #Ignore SIG_PIPE and don't throw exceptions on it... (http://docs.python.org/library/signal.html)
 signal(SIGPIPE,SIG_DFL) 
