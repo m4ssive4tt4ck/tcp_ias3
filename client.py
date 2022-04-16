b1a8b60ce80a5aabf61efc6c6c8d2cefce744b7e
m4ssive4tt4ck <rahelkempf@gmx.ch>
2022-04-16 13:00:24 +0200

    remove sigpipe ignore

--- client.py
+++ client.py
@@ -1,7 +1,3 @@
-from signal import signal, SIGPIPE, SIG_DFL 
-#Ignore SIG_PIPE and don't throw exceptions on it... (http://docs.python.org/library/signal.html)
-signal(SIGPIPE,SIG_DFL) 
-
 import sys
 import socket
 import select
