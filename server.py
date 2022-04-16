5b9af50f3d353161c5c8134c235614360ab85b11
m4ssive4tt4ck <rahelkempf@gmx.ch>
2022-04-16 12:53:58 +0200

    ignore sigpipe error

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+from signal import signal, SIGPIPE, SIG_DFL 
+#Ignore SIG_PIPE and don't throw exceptions on it... (http://docs.python.org/library/signal.html)
+signal(SIGPIPE,SIG_DFL) 
+
 import socket
 import sys
 import select
